e762f053a8766bd62cfb501eabc5320c7c9a0c9a
Ivan Evtimov <ivanevtimov5@gmail.com>
2016-12-02 22:21:59 -0500

    y axis okay

--- svg_barchart/script.js
+++ svg_barchart/script.js
@@ -10,9 +10,26 @@ d3.csv('data.csv', row, function(error, data){
     //space = barWidth/5; //the space is for the grouping
     //barWidth = (width - (data.length/2)*space)/data.length;
 
+    var xScaleRange = [0];
+    var i = 0;
+    for(i = barWidth; i < width; i += 2*(barWidth+space))
+    {
+        xScaleRange.push(i)
+    }
+    xScaleRange.push((i-barWidth-2*space));
+
+    var xScaleDom = data.map(function(d){return (+d.year)+'/'+(+d.year+1);})
+    xScaleDom = [''].concat(xScaleDom)
+    xScaleDom = xScaleDom.concat([''])
+
+    var xScale = d3.scale.ordinal()
+                .domain(xScaleDom)
+                .range(xScaleRange)
+
     var yScale = d3.scale.linear()
     .range([0, height])
     .domain([0, d3.max(data, function(d){return +d.value;})]);
+
 
 
     var chart = d3.select('#chart')
@@ -31,7 +48,7 @@ d3.csv('data.csv', row, function(error, data){
 
     bar.append("rect")
         .attr("height", function(d){return yScale(d.value)})
-        .attr("width", barWidth-1)
+        .attr("width", barWidth)
         .attr("fill", function(d){
             if (d.school === 'Trinity East El Sch')
             {
@@ -44,13 +61,22 @@ d3.csv('data.csv', row, function(error, data){
 
         });
 
-    bar.append("text")
-        .attr("y", function(d){ return height - yScale(d.value) + 20;})
-        .attr("x", 0)
-        .attr('fill', 'white')
-        .attr('font', '5 sans-serif')
-        .text(function(d){ return d.value;});
-      });
+//    bar.append("text")
+//        .attr("y", function(d){ return height - yScale(d.value) + 20;})
+//        .attr("x", 0)
+//        .attr('fill', 'white')
+//        .attr('font', '5 sans-serif')
+//        .text(function(d){ return d.value;});
+
+    var xAxis = d3.svg.axis()
+                .scale(xScale)
+                .orient("bottom")
+
+    chart.append('g')
+        .attr('class', 'x axis')
+        .attr('transform', 'translate(' + space +','  + height +')' )
+        .call(xAxis)
+});
 
 function row(d){
    return {
